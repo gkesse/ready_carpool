dc01d148b8a9c6d26391e638ec068d2f0badb7d8
Gerard KESSE <tiakagerard@hotmail.com>
2018-05-07 22:15:11 +0200

    Initial Commit

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <div class="Parallax">
         <div class="Img">
             <div class="Caption">
-                <span class="Text">ACCUEIL</span>
+                <div class="Text">ACCUEIL</div>
             </div>
         </div>
         <div class="Body">
@@ -25,15 +25,27 @@
         </div>
     </div>
     <!-- ============================================ -->
-    <div class="Parallax2">
+    <div class="Parallax Img2 Bgr2">
         <div class="Img">
             <div class="Caption">
-                <span class="Text">PRESENTATION</span>
+                <div class="Text">PRESENTATION</div>
             </div>
         </div>
         <div class="Body">
             <div>Eglise Harriste de France</div>
             <div>Accueil...</div>
+        </div>
+    </div>
+    <!-- ============================================ -->
+    <div class="Parallax Img3 Bgr3">
+        <div class="Img">
+            <div class="Caption">
+                <div class="Text">MINISTERE</div>
+            </div>
+        </div>
+        <div class="Body">
+            <div>Eglise Harriste de France</div>
+            <div>MINISTERE...</div>
         </div>
     </div>
     <!-- ============================================ -->
