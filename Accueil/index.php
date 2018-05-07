8572936e59a5dcc0e3b63255a999507a9ffd940e
Gerard KESSE <tiakagerard@hotmail.com>
2018-05-07 17:44:18 +0200

    Initial Commit

---
+++ Accueil/index.php
@@ -0,0 +1,10 @@
+<?php 
+	require $_SERVER["DOCUMENT_ROOT"]."/php/header.php"; 
+?>
+<!-- ============================================ -->
+<div>
+	<h1>Eglise Harriste de France</h1>
+</div>
+<!-- ============================================ -->
+<?php require $_SERVER["DOCUMENT_ROOT"]."/php/footer.php"; ?>
+<!-- ============================================ -->
---
+++ php/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+		<link rel="stylesheet" href="/css/style.css"/>
+		<script src="js/GHeader.js"></script> 
+		<script src="js/header.js"></script> 
+	</head>
+	<body>
+		<div class="Header">
+			<!-- ============================================ -->
+			<div class="HeaderMenu">
+				<a href="#">Menu</a>
+				<a href="#">Menu</a>
+				<a href="#">Menu</a>
+				<a href="#">Menu</a>
+			</div>
+			<!-- ============================================ -->
+		</div>
