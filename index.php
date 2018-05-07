9096a82e3d3b03ee4d0ceac24bd570966b10c40c
Gerard KESSE <tiakagerard@hotmail.com>
2018-05-07 20:40:11 +0200

    Initial Commit

--- index.php
+++ index.php
@@ -2,8 +2,27 @@
 	require $_SERVER["DOCUMENT_ROOT"]."/php/header.php"; 
 ?>
 <!-- ============================================ -->
-<div>
-	<h1>Eglise Harriste de France</h1>
+<div class="MainPage">
+    <!-- ============================================ -->
+    <div class="Header">
+        <div class="HeaderMenu" id="HeaderMenuId">
+            <div class="menu"><a class="item" href="#">Accueil</a></div>
+            <div class="menu"><a class="item" href="#">Presentation</a></div>
+            <div class="menu"><a class="item" href="#">Ministère</a></div>
+            <div class="menu icon" onclick="openHeaderMenu(this)"><i class="fa fa-bars"></i></div>
+        </div>
+    </div>
+    <!-- ============================================ -->
+    <div class="ParallaxImg" style='background-image: url("/img/img_parallax.jpg");'></div>
+    <div class="ParallaxCtn">
+        Acceuil
+    </div>
+    <!-- ============================================ -->
+    <div class="ParallaxImg2" style='background-image: url("/img/img_parallax2.jpg");'></div>
+    <div class="ParallaxCtn2">
+        Presentation
+    </div>
+    <!-- ============================================ -->
 </div>
 <!-- ============================================ -->
 <?php require $_SERVER["DOCUMENT_ROOT"]."/php/footer.php"; ?>
--- php/header.php
+++ php/header.php
@@ -4,20 +4,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
         <link rel="stylesheet" href="/css/style.css"/>
-    </head>
-    <body>
-        <div class="Header">
-            <!-- ============================================ -->
-            <div class="HeaderMenu" id="HeaderMenuId">
-                <a class="item" href="#">Accueil</a>
-                <a class="item" href="#">Presentation</a>
-                <a class="item" href="#">Ministère</a>
-                <span class="item icon" onclick="openHeaderMenu(this)"><i class="fa fa-bars"></i></span>
-            </div>
-            <!-- ============================================ -->
-        </div>
-        <!-- ============================================ -->
         <script src="/js/class/GHeader.js"></script> 
         <script src="/js/header.js"></script> 
+    </head>
+    <body>
         <!-- ============================================ -->
         
