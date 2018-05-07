bb666dfb7de0f0c21c60527d9ac8815ffdd624b0
Gerard KESSE <tiakagerard@hotmail.com>
2018-05-07 18:34:57 +0200

    Initial Commit

--- js/class/GHeader.js
+++ js/class/GHeader.js
@@ -1,51 +1,23 @@
 //===============================================
 var GHeader = (function() {
     //===============================================
-    var m_instance;
+    var linstance;
     //===============================================
     var Container = function() {
         return {
             //===============================================
             openHeaderMenu: function(obj) {
-				var m_HeaderMenu = document.getElementById("HeaderMenu");
-				var m_bars = '<i class="fa fa-bars"></i>';
-				if(m_bars == obj.innerHTML) {
-					m_bars = '<i class="fa fa-close"></i>';
-					m_HeaderMenu.className += " HeaderMenuRwd";
+				var lHeaderMenu = document.getElementById("HeaderMenuId");
+				var lBars = '<i class="fa fa-bars"></i>';
+				if(obj.innerHTML == lBars) {
+					lBars = '<i class="fa fa-close"></i>';
+					lHeaderMenu.className += " RWD";
 				}
 				else {
-					var m_className = m_HeaderMenu.className;
-					m_HeaderMenu.className = m_className.replace(" HeaderMenuRwd", "");
+					var lClassName = lHeaderMenu.className;
+					lHeaderMenu.className = lClassName.replace(" RWD", "");
 				}
-				obj.innerHTML = m_bars;	
-            },
-            //===============================================
-            openDisconnect: function() {
-				var m_Logout = document.getElementById("Logout");
-				var m_LogoutCtn = document.getElementById("LogoutCtn");
-				var m_className = m_LogoutCtn.className;
-				m_LogoutCtn.className = m_className.replace(" ModalAnimHide", " ModalAnimShow");
-				m_Logout.style.display = "block";
-            },
-            //===============================================
-            disconnectClose: function() {
-				var m_Logout = document.getElementById("Logout");
-				var m_LogoutCtn = document.getElementById("LogoutCtn");
-				var m_className = m_LogoutCtn.className;
-				m_LogoutCtn.className = m_className.replace(" ModalAnimShow", " ModalAnimHide");
-				setTimeout(function() {
-					m_Logout.style.display = "none";
-				}, 400);
-            },
-            //===============================================
-            windowEvent: function() {
-				window.onclick = function(event) {
-					var m_Logout = document.getElementById("Logout");
-					var m_target = event.target;
-					if(m_target == m_Logout) {
-						disconnectClose();
-					}
-				};
+				obj.innerHTML = lBars;	
             }
             //===============================================
         };
@@ -53,14 +25,12 @@ var GHeader = (function() {
     //===============================================
     return {
         Instance: function() {
-            if (!m_instance) {
-                m_instance = Container();
+            if (!linstance) {
+                linstance = Container();
             }
-            return m_instance;
+            return linstance;
         }
     };
     //===============================================
 })();
-//===============================================
-GHeader.Instance().windowEvent();
 //===============================================
--- php/header.php
+++ php/header.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-		<link rel="stylesheet" href="/css/style.css"/>
-		<script src="js/GHeader.js"></script> 
-		<script src="js/header.js"></script> 
-	</head>
-	<body>
-		<div class="Header">
-			<!-- ============================================ -->
-			<div class="HeaderMenu">
-				<a class="item" href="#">Accueil</a>
-				<a class="item" href="#">Presentation</a>
-				<a class="item" href="#">Ministère</a>
-                <span class="item icon"><i class="fa fa-bars"></i></span>
-			</div>
-			<!-- ============================================ -->
-		</div>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+        <link rel="stylesheet" href="/css/style.css"/>
+    </head>
+    <body>
+        <div class="Header">
+            <!-- ============================================ -->
+            <div class="HeaderMenu" id="HeaderMenuId">
+                <a class="item" href="#">Accueil</a>
+                <a class="item" href="#">Presentation</a>
+                <a class="item" href="#">Ministère</a>
+                <span class="item icon" onclick="openHeaderMenu(this)"><i class="fa fa-bars"></i></span>
+            </div>
+            <!-- ============================================ -->
+        </div>
+        <!-- ============================================ -->
+        <script src="/js/class/GHeader.js"></script> 
+        <script src="/js/header.js"></script> 
+        <!-- ============================================ -->
+        
