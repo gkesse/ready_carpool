eb94d0a625dad9d929576c25e3c934180afc0672
Gerard KESSE <tiakagerard@hotmail.com>
2018-05-08 10:54:23 +0200

    Initial Commit

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
     <div class="Header">
         <div class="HeaderMenu" id="HeaderMenuId">
             <div class="menu"><a class="item" href="#">Accueil</a></div>
-            <div class="menu"><a class="item" href="#">Presentation</a></div>
+            <div class="menu"><a class="item" href="#">Présentation</a></div>
             <div class="menu"><a class="item" href="#">Ministère</a></div>
+            <div class="menu"><a class="item" href="#">Evènements</a></div>
             <div class="menu icon" onclick="openHeaderMenu(this)"><i class="fa fa-bars"></i></div>
         </div>
     </div>
