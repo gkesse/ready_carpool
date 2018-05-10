9b0eb29fad48bd0300aa0ac862055208d9174f98
Gerard KESSE <tiakagerard@hotmail.com>
2018-05-10 19:16:07 +0200

    Initial Commit

--- js/class/GConnection.js
+++ js/class/GConnection.js
@@ -40,10 +40,11 @@ var GConnection = (function() {
                 }
                 
                 if(lMessage.length) {
-                    var lHtml = "<i class='fa fa-book'></i> "; 
+                    var lHtml = "<i class='fa fa-exclamation-triangle'></i> "; 
                     lHtml += lMessage; 
                     lConnectionMsg.innerHTML = lHtml;
                     lConnectionMsg.style.display = "block";
+                    lConnectionMsg.style.color = "#ff9933";
                 }
                 else {
                     this.sendConnection(lEmail.value, lPassword.value);
@@ -52,16 +53,25 @@ var GConnection = (function() {
             //===============================================
             sendConnection: function(email, pass) {
 				var lConnectionMsg = document.getElementById("ConnectionMsg");
+                lConnectionMsg.style.display = "none";
                 var lXmlhttp = new XMLHttpRequest();
                 lXmlhttp.onreadystatechange = function() {
                     if(this.readyState == 4 && this.status == 200) {
                         var lData = this.responseText;
                         var lDataMap = JSON.parse(lData);
                         if(!lDataMap["STATUS"]) {
-                            var lHtml = "<i class='fa fa-book'></i> "; 
+                            var lHtml = "<i class='fa fa-exclamation-triangle'></i> "; 
                             lHtml += lDataMap["MSG"]; 
                             lConnectionMsg.innerHTML = lHtml;
                             lConnectionMsg.style.display = "block";
+                            lConnectionMsg.style.color = "#ff9933";
+                        }
+                        else {
+                            var lHtml = "<i class='fa fa-check-circle'></i> "; 
+                            lHtml += lDataMap["MSG"]; 
+                            lConnectionMsg.innerHTML = lHtml;
+                            lConnectionMsg.style.display = "block";
+                            lConnectionMsg.style.color = "#339933";
                         }
                     }
                 }
